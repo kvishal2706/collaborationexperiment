8f6cd260e7c0e6e4412f9b1c895fcbd0b16135fe
karannaik503 <karannaik503@gmail.com>
2024-09-13 17:12:36 +0530

    Added function ‘matrix_multiply’

--- example.py
+++ example.py
@@ -1,1 +1,18 @@
 "Initial commit"
+
+def matrix_multiply(A, B):
+    rows_A, cols_A = len(A), len(A[0])
+    rows_B, cols_B = len(B), len(B[0])
+
+    if cols_A != rows_B:
+        raise ValueError("Number of columns in A must be equal to the number of rows in B.")
+
+    result = [[0 for _ in range(cols_B)] for _ in range(rows_A)]
+
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                result[i][j] += A[i][k] * B[k][j]
+
+    return result
+
