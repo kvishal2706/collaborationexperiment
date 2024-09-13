e41d3d6e1154307f2727b3c8355897b32138c249
deveshksh <dkshandilya12345@gmail.com>
2024-09-13 20:27:43 +0530

    Insertion Sort

--- example.py
+++ example.py
@@ -1,6 +1,23 @@
-"Initial commit"
-def print_message():
-    print("Hello from Devesh's branch!")
+# --------------------------------------------------------------
+#  author @devesh
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
 
-print_message()
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
+def printArray(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6]
+    insertionSort(arr)
+    printArray(arr)
+
+# --------------------------------------------------------------
