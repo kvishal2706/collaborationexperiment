52a161549cdd666715e5b59c4c5900d85f38c613
Mourya-K <mourya.k2112@gmail.com>
2024-09-13 20:50:26 +0530

    Added Bubble Sort Code - Mourya

--- example.py
+++ example.py
@@ -1,25 +1,12 @@
-"Initial commit"
+#   author @mourya - Code for Bubble Sort Algorithm
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
-# Mourya Kakarapu
-print('Messi GOAT')
-#author geeta
-def add(a ,b):
-    return a+b
-"Initial commit"
-def print_message():
-    print("Hello from Devesh's branch!")
-
-print_message()
-
-print("sujith has contributed")
-print("Sriram")
-# Aayushi Sinha
-def Fibonacci(n):
-	if n<= 0:
-		print("Incorrect input")
-	elif n == 1:
-		return 0
-	elif n == 2:
-		return 1
-	else:
-		return Fibonacci(n-1)+Fibonacci(n-2)
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bubble_sort(arr)
+print("Sorted array:", sorted_arr)
